940531ca6e16b62265a70865f9251d8c2f4dbe36
Oksana Sheremet <sheremet5631@gmail.com>
2022-09-21 10:32:16 +0300

    Added actions

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from .models import Ingredient, Recipe, Tag
 from users.models import User
 
 
